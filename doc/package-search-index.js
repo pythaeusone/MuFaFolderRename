931408c509be2f56c29abad8e3054798245ea085
Pythaeus by MuFa <mustafa.jukic@gfn.education>
2022-10-31 20:13:27 +0100

    remove doc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.mufa.controller"},{"l":"de.mufa.main"},{"l":"de.mufa.model"},{"l":"de.mufa.view"}];updateSearchResults();
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"de.mufa.controller","l":"ButtonColor_C"},{"p":"de.mufa.main","l":"Main"},{"p":"de.mufa.controller","l":"Main_C"},{"p":"de.mufa.model","l":"Main_M"},{"p":"de.mufa.view","l":"Main_V"}];updateSearchResults();
