ad23f66710acf0eed7b46a413161f25cbe61112d
Pythaeus by MuFa <mustafa.jukic@gfn.education>
2022-10-31 19:33:30 +0100

    JavaDoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.mufa.controller"},{"l":"de.mufa.main"},{"l":"de.mufa.model"},{"l":"de.mufa.view"}];updateSearchResults();
